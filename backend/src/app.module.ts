13bb5520facdf7902626aed25fb87ed2ba456a83
Valentin Heiserer <valentin@heiserer.de>
2026-03-08 01:22:50 +0100

    feat(backend): :sparkles: add configuration module to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { DocumentController } from './controllers/document.controller';
 import { DocumentService } from './services/document.service';
 import { LoggingRepository } from './repositories/logging.repository';
+import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: configValidationSchema,
+      envFilePath: '../.env',
+    }),
+  ],
   controllers: [DocumentController],
   providers: [
     DocumentService,
