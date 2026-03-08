a93a8625fb52c88b02d2f3c7bdcc499f78086ada
Valentin Heiserer <valentin@heiserer.de>
2026-03-08 16:24:30 +0100

    fix(backend): :art: use logger in nest bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const bootLogger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
   const configService = app.get(ConfigService<any, true>);
-  console.log(`Starting server on port ${configService.get<number>('PORT')}`);
+  const port = configService.get<number>('PORT');
 
-  await app.listen(configService.get<number>('PORT'));
+  bootLogger.log(`Starting server on port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
